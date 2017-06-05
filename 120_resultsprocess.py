d186e5d9335ad07adca06460a70b2b331672e26e
metcalfalex <alex.metcalf@uqconnect.edu.au>
2017-06-05 19:58:16 +1000

    scoring working

--- 120_resultsprocess.py
+++ 120_resultsprocess.py
@@ -6,20 +6,14 @@ def lambda_handler(event, context):
     obj = s3.Object('20170602whichdog','110_resultsraw.csv')
     response = obj.get()
     lines = response[u'Body'].read().decode('utf-8').split('\n')
-    # csvfile = csv.reader(lines, delimiter='|')
     csvfile = csv.DictReader(lines, delimiter='|',fieldnames=['date','dogyes','dogno'])
-    a1 = [row['dogyes'] for row in csvfile]
-    # data = [row for row in csvfile]
-    # lines = response[u'Body'].read().split()
-    # a1 = [row['dogyes'] for row in csv.DictReader(lines, delimiter='|',fieldnames=['date','dogyes','dogno'])]
-    # a1 = [row['dogyes'] for row in csv.DictReader(lines, delimiter='|',fieldnames=['date','dogyes','dogno'])]
-    # for row in csv.DictReader(response['Body'], delimiter='|',fieldnames=['date','dogyes','dogno']):
-    # for i, row in csv.DictReader(lines, delimiter='|',fieldnames=['date','dogyes','dogno']):
-        # a1[i] = row['dogyes']
-        # a1 = 'yes'
-    # return None
-    # return str(lines)
-    return a1
-    # return str(csvfile)
-    # return str(response['Body'])
-    # return str(response)
+    dogyes = [int(row['dogyes']) for row in csvfile]
+    csvfile = csv.DictReader(lines, delimiter='|',fieldnames=['date','dogyes','dogno'])
+    dogno = [int(row['dogno']) for row in csvfile]
+    dogall = sorted(set(dogyes + dogno))
+    dogyes_count = [dogyes.count(n) for n in dogall]
+    dogall_count = [dogyes.count(n) + dogno.count(n) for n in dogall]
+    score = [(dogyes_count[i] - dogall_count[i]/2 + 5)/10 if dogall_count[i] < 10 else dogyes_count[i] / dogall_count[i] for i, n in enumerate(dogall)]
+    # output to json and save to s3: dogall | dogyes_count | dogall_count | score
+    # js json to table: http://jsfiddle.net/URU5G/1/
+    return score
