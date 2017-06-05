3b8d110f75be026310e7f555f8934a7f4c78477c
metcalfalex <alex.metcalf@uqconnect.edu.au>
2017-06-05 18:52:14 +1000

    get not working

--- 120_resultsprocess.py
+++ 120_resultsprocess.py
@@ -1,6 +1,18 @@
+import csv
 import boto3
 
 def lambda_handler(event, context):
-	s3 = boto3.resource('s3')
-	raw = s3.meta.client.get_object(Bucket='20170602whichdog',Key='110_resultsraw.csv')
-	return raw.read(10)
+    s3 = boto3.resource('s3')
+    obj = s3.Object('20170602whichdog','110_resultsraw.csv')
+    response = obj.get()
+    lines = response[u'Body'].read()
+    # lines = response[u'Body'].read().split()
+    # a1 = [row['dogyes'] for row in csv.DictReader(lines, delimiter='|',fieldnames=['date','dogyes','dogno'])]
+    # a1 = [row['dogyes'] for row in csv.DictReader(lines, delimiter='|',fieldnames=['date','dogyes','dogno'])]
+    # for row in csv.DictReader(response['Body'], delimiter='|',fieldnames=['date','dogyes','dogno']):
+        # a1 = row['dogyes']
+        # a1 = 'yes'
+    return str(lines)
+    # return a1
+    # return str(response['Body'])
+    # return str(response)
