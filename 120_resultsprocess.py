5eddb6da28f886aa76afa0014ce7db093cbff86c
metcalfalex <alex.metcalf@uqconnect.edu.au>
2017-06-05 19:25:22 +1000

    get working

--- 120_resultsprocess.py
+++ 120_resultsprocess.py
@@ -5,14 +5,21 @@ def lambda_handler(event, context):
     s3 = boto3.resource('s3')
     obj = s3.Object('20170602whichdog','110_resultsraw.csv')
     response = obj.get()
-    lines = response[u'Body'].read()
+    lines = response[u'Body'].read().decode('utf-8').split('\n')
+    # csvfile = csv.reader(lines, delimiter='|')
+    csvfile = csv.DictReader(lines, delimiter='|',fieldnames=['date','dogyes','dogno'])
+    a1 = [row['dogyes'] for row in csvfile]
+    # data = [row for row in csvfile]
     # lines = response[u'Body'].read().split()
     # a1 = [row['dogyes'] for row in csv.DictReader(lines, delimiter='|',fieldnames=['date','dogyes','dogno'])]
     # a1 = [row['dogyes'] for row in csv.DictReader(lines, delimiter='|',fieldnames=['date','dogyes','dogno'])]
     # for row in csv.DictReader(response['Body'], delimiter='|',fieldnames=['date','dogyes','dogno']):
-        # a1 = row['dogyes']
+    # for i, row in csv.DictReader(lines, delimiter='|',fieldnames=['date','dogyes','dogno']):
+        # a1[i] = row['dogyes']
         # a1 = 'yes'
-    return str(lines)
-    # return a1
+    # return None
+    # return str(lines)
+    return a1
+    # return str(csvfile)
     # return str(response['Body'])
     # return str(response)
